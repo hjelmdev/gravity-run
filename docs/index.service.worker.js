780f106a4f3a413d10068d3b462f26f953755a3f
Adam Hjelm <hjelm.adam@gmail.com>
2026-09-24 22:14:49 +0200

    Make web canvas adaptive and handle browser exit

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790280613|1935587';
+const CACHE_VERSION = '1790280860|1956842';
 /** @type {string} */
 const CACHE_PREFIX = 'Gravity Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
