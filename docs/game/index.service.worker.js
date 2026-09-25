a7379f131f412b93b8c8bb62e32c77b6e04496b6
Adam Hjelm <hjelm.adam@gmail.com>
2026-09-25 15:56:31 +0200

    Fix leaderboard decompression in web export

--- docs/game/index.service.worker.js
+++ docs/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790343862|1946661';
+const CACHE_VERSION = '1790344561|1951389';
 /** @type {string} */
 const CACHE_PREFIX = 'Gravity Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
