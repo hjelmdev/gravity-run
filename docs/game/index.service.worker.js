526d34af74168c736518769f0e0ce91a83ae067f
Adam Hjelm <hjelm.adam@gmail.com>
2026-09-25 15:37:23 +0200

    Fix mobile layout and orientation shell

--- docs/game/index.service.worker.js
+++ docs/game/index.service.worker.js
@@ -4,14 +4,14 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790328297|1913748';
+const CACHE_VERSION = '1790343345|1898723';
 /** @type {string} */
 const CACHE_PREFIX = 'Gravity Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
-const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
+const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 // Files that will be cached on load.
 /** @type {string[]} */
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
