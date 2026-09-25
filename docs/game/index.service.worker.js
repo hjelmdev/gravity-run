14bf81c2af64c8d5e928b641cd607c88da868c19
Adam Hjelm <hjelm.adam@gmail.com>
2026-09-25 15:45:18 +0200

    Refresh web deployment and fit leaderboard

--- docs/game/index.service.worker.js
+++ docs/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790343345|1898723';
+const CACHE_VERSION = '1790343862|1946661';
 /** @type {string} */
 const CACHE_PREFIX = 'Gravity Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
